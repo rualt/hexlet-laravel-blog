bdf5e81b6837a7374b1824b86257d2157f627bfd
rualt <rualnar@gmail.com>
2019-12-06 16:11:51 +0400

    add actions create and store

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -10,6 +11,33 @@ class ArticleController extends Controller
     {   
         $articles = Article::paginate();
         return view('article.index', compact('articles'));
+    }
+
+    public function create()
+    {
+        $article = new Article();
+        return view('article.create', compact('article'));
+    }
+
+    // Здесь нам понадобится объект запроса для извлечения данных
+    public function store(Request $request)
+    {
+        // Проверка введённых данных
+        // Если будут ошибки, то возникнет исключение
+        $this->validate($request, [
+            'name' => 'required|unique:articles',
+            'body' => 'required|min:1000',
+        ]);
+
+        $article = new Article();
+        // Заполнение статьи данными из формы
+        $article->fill($request->all());
+        // При ошибках сохранения возникнет исключение
+        $article->save();
+
+        // Редирект на указанный маршрут с добавлением флеш-сообщения
+        return redirect()
+            ->route('articles.index');
     }
 
     public function show($id)
