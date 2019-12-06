c0b541f5c7d567c4cf45727a7382e59693e9d46b
rualt <rualnar@gmail.com>
2019-12-06 16:22:05 +0400

    add routes create and store for articles

--- routes/web.php
+++ routes/web.php
@@ -15,27 +15,16 @@
 Route::get('/', 'PageController@home');
 Route::get('/about', 'PageController@about');
 
+
 Route::get('/articles', 'ArticleController@index')
     ->name('articles.index');
+
+Route::get('/articles/create', 'ArticleController@create')
+    ->name('articles.create');
+
+Route::post('/articles', 'ArticleController@store')
+    ->name('articles.store');
 
 Route::get('/articles/{id}', 'ArticleController@show')
     ->name('articles.show');
 
-
-/* $team = [
-    ['name' => 'Luke Skywalker', 'position' => "CEO's son"],
-    ['name' => 'C-3PO', 'position' => 'protocol droid'],
-    ['name' => 'R2-D2', 'position' => 'astromech droid'],
-    ['name' => 'Darth Vader', 'position' => 'CEO']
-];
-
-$articles = \App\Article::all();
-
-Route::get('/articles', function () use ($articles) {
-    return view('articles', compact('articles'));
-});
-
-
-Route::get('/about', function () use ($team) {
-    return view('about', compact('team'));
-}); */
