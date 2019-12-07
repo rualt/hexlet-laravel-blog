2fed90b4960fb7ba4463ef8b4ad1f882aa018e00
rualt <rualnar@gmail.com>
2019-12-07 22:04:12 +0400

    fix elements on the atticle page

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,9 +5,11 @@
 
     <h1>Список статей</h1>
     @foreach ($articles as $article)
-        <h2><a href="{{ route('articles.show', $article->id)}}">{{$article->name}}</a></h2>
-        <div>{{Str::limit($article->body, 200)}}</div>
-        <td><a href="{{route('articles.edit', $article->id)}}">Edit</td></br>
-        <td><a href="{{route('articles.destroy', $article->id)}}" data-confirm="Вы уверены?" data-method="delete">Delete</td>
+        </br>{{$article->id}}</td>
+        <a href="{{route('articles.show', $article->id)}}">{{$article->name}}</a></td></br>
+        <div>{{Str::limit($article->body, 100)}}</div></br>
+        <a href="{{route('articles.edit', $article->id)}}">Edit
+        <a href="{{route('articles.destroy', $article->id)}}" data-confirm="Вы уверены?" data-method="delete">Delete
+        </br>
     @endforeach
 @endsection
