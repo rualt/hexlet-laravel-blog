ae8e3ffea9b38d5696f7b5d08258dcc66700a436
rualt <rualnar@gmail.com>
2019-12-07 23:33:41 +0400

    add delete link to article page

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,5 +4,7 @@
     <h1>{{$article->name}}</h1>
     <div>{{$article->body}}</div>
     <a href="{{ route('articles.edit', $article->id)}}">Редактировать</a>
+    <a href="{{route('articles.destroy', $article->id)}}" data-confirm="Вы уверены?" data-method="delete" rel="nofollow">Удалить</a>
+    <input type="submit" value="Сохранить" data-disable-with="Сохраняем">
 @endsection
 
