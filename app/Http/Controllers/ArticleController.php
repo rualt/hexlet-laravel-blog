256b0afee21f0f185ffd4678cf67193269296c45
rualt <rualnar@gmail.com>
2019-12-08 20:53:00 +0400

    implement resoure routing

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,76 +7,103 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
-    {   
+    {
         $articles = Article::paginate();
         return view('article.index', compact('articles'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         $article = new Article();
         return view('article.create', compact('article'));
     }
 
-    public function edit($id)
-    {
-        $article = Article::findOrFail($id);
-        return view('article.edit', compact('article'));
-    }
-
-    public function update(Request $request, $id)
-    {
-        $article = Article::findOrFail($id);
-        $this->validate($request, [
-            // У обновления немного изменённая валидация. 
-            // В проверку уникальности добавляется название поля и id текущего объекта
-            // Если этого не сделать, Laravel будет ругаться на то что имя уже существует
-            'name' => 'required|unique:articles,name,' . $article->id,
-            'body' => 'required|min:100',
-        ]);
-
-        $article->fill($request->all());
-        $article->save();
-        return redirect()
-            ->route('articles.index');
-    }
-
-    // Здесь нам понадобится объект запроса для извлечения данных
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        // Проверка введённых данных
-        // Если будут ошибки, то возникнет исключение
         $this->validate($request, [
             'name' => 'required|unique:articles',
             'body' => 'required|min:1000',
         ]);
 
         $article = new Article();
-        // Заполнение статьи данными из формы
         $article->fill($request->all());
-        // При ошибках сохранения возникнет исключение
         $article->save();
 
-        // Редирект на указанный маршрут с добавлением флеш-сообщения
         return redirect()
             ->route('articles.index')
             ->with('success', 'Article created successfully');
     }
 
-    public function show($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
     {
-        $article = Article::findOrFail($id);
         return view('article.show', compact('article'));
     }
 
-    public function destroy($id)
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
     {
-        $article = Article::find($id);
+        return view('article.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:articles,name,' . $article->id,
+            'body' => 'required|min:100',
+        ]);
+        $article->fill($request->all());
+        $article->save();
+        
+        return redirect()
+            ->route('articles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
         if ($article) {
             $article->delete();
         }
         return redirect()->route('articles.index');
     }
-
-}   
+}
--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,4 @@ Route::get('/', 'PageController@home');
 
 Route::get('/about', 'PageController@about');
 
-
-Route::get('/articles', 'ArticleController@index')
-    ->name('articles.index');
-
-Route::get('/articles/create', 'ArticleController@create')
-    ->name('articles.create');
-
-Route::post('/articles', 'ArticleController@store')
-    ->name('articles.store');
-
-Route::get('/articles/{id}/edit', 'ArticleController@edit')
-->name('articles.edit');
-
-Route::patch('/articles/{id}', 'ArticleController@update')
-  ->name('articles.update');
-
-Route::get('/articles/{id}', 'ArticleController@show')
-    ->name('articles.show');
-
-Route::delete('/articles/{id}', 'ArticleController@destroy')
-  ->name('articles.destroy');
+Route::resource('/articles', 'ArticleController');
